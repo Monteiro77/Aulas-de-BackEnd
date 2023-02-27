d8c1d48d250c8ffe1311e9d76bd141f5f862b2b9
Vinícius Monteiro <vmonteiro7843@gmail.com>
2023-02-27 16:59:17 -0300

    AULA 7 pt2

--- AULA 07/app.js
+++ AULA 07/app.js
@@ -13,60 +13,64 @@ const listaNomes = ['Cletion', 'Ronaldo', 'Matuê', 'Wiu', 'Teto'];
 
 const listaProdutos = ['Mouse', 'Teclado', 'Monitor', 'Mousepad', 'Gabinete', 'HD', 'Memória RAM']
 
+/**
+ * JSON É COMPOSTO POR :chave ( atributo ) e valor
+ * EXEMPLO:  {nome: 'José', celular : '11989829145' , email : 'josémuitocria@gmailcom'}
+ * 
+ */
+const exibirDados = function () {
+    console.log(listaNomes)
 
-const exibirDados = function(){
-console.log(listaNomes)
+    //Verifica o tipo de tatos do índice
+    // conosle.log(typeof(listaNomes))
 
-//Verifica o tipo de tatos do índice
-// conosle.log(typeof(listaNomes))
+    console.log('O melhor cantor do mundo é o ' + listaNomes[2])
 
-console.log('O melhor cantor do mundo é o ' + listaNomes[2])
+    //Table - permite vizuaizar o conteúdo do array em tabela
+    console.table(listaNomes)
 
-//Table - permite vizuaizar o conteúdo do array em tabela
-console.table(listaNomes)
-
-// Como retornar a quantidade de intens dentro de uma array
-console.log(listaNomes.length)
+    // Como retornar a quantidade de intens dentro de uma array
+    console.log(listaNomes.length)
 
 
-//Percorrendo um array com while
-let cont = 0; // Starts
-let qtdeItens = listaNomes.length; //End
+    //Percorrendo um array com while
+    let cont = 0; // Starts
+    let qtdeItens = listaNomes.length; //End
 
-console.log('***************EXEMPLO COM WHILE****************')
+    console.log('***************EXEMPLO COM WHILE****************')
 
-while(cont < qtdeItens){
-    console.log('Nome do aluno: ' + listaNomes[cont])
-    cont += 1;
+    while (cont < qtdeItens) {
+        console.log('Nome do aluno: ' + listaNomes[cont])
+        cont += 1;
+    }
+
+    console.log('***************FIM DO EXEMPLO****************')
+
+    console.log('\n')
+
+    console.log('***************EXEMPLO COM FOR****************')
+
+    for (let cont = 0; cont < qtdeItens; cont += 1) {
+        console.log('Nome do aluno: ' + listaNomes[cont])
+
+    }
+
+    console.log('***************FIM DO EXEMPLO****************')
+
+    console.log('\n')
+
+
+    console.log('***************EXEMPLO COM FOR EACH****************')
+    listaNomes.forEach(function (nome) {
+        console.log('Nome do aluno: ' + nome)
+    });
+
+    console.log('********************FIM DO EXEMPLO**************************')
+
+
 }
 
-console.log('***************FIM DO EXEMPLO****************')
-
-console.log('\n')
-
-console.log('***************EXEMPLO COM FOR****************')
-
-for(let cont = 0; cont < qtdeItens; cont+= 1){
-    console.log('Nome do aluno: ' + listaNomes[cont])
-
-}
-
-console.log('***************FIM DO EXEMPLO****************')
-
-console.log('\n')
-
-
-console.log('***************EXEMPLO COM FOR****************')
-listaNomes.forEach(function(nome){
-    console.log('Nome do aluno: ' + nome)
-});
-
-console.log('********************FIM DO EXEMPLO**************************')
-
-
-}
-
-const manipulandoDados = function(){
+const manipulandoDados = function () {
     //push - adciona novos itens no final do array
     listaProdutos.push('Memória')
     listaProdutos.push('Gabinet', 'Placa mãe', 'Processador')
@@ -87,63 +91,160 @@ const manipulandoDados = function(){
 
 
     //slice- permite criar uma nova cópia de outro array
-    const novoProdutos =listaProdutos.slice();
+    const novoProdutos = listaProdutos.slice();
     console.log('******************************')
     console.table(novoProdutos)
 
 
     //indexOf - Permite mostrar o indice de m item
-        //se o retorno fr -1, o item não existe
-        //se o retorno foi >= que 0 o item existe
-    
+    //se o retorno fr -1, o item não existe
+    //se o retorno foi >= que 0 o item existe
+
     listaProdutos.indexOf('Computer')
 
     //Exemplo de utilização do IndexOf
-    if(listaProdutos.indexOf >= 0){
+    if (listaProdutos.indexOf >= 0) {
         console.log('Item encontrado')
-    }else{
-        
+    } else {
+
         console.log('Item não encontrado')
     }
-    
+
 };
 
-const removendoProdutos = function(nomeProduto){
+const removendoProdutos = function (nomeProduto) {
     let nome = nomeProduto
     let indice = listaProdutos.indexOf(nome);
     let status = false
 
     //splice - remove itens do array com base índice
     //obs : devemos encaminhar um segurndo parâmetro para determinarmos quantos elementos ele vai apagar para baixo
-    if(indice >= 0 ){
+    if (indice >= 0) {
         listaProdutos.splice(indice, 1)
         status = true
 
-    }else
-    status =  false
+    } else
+        status = false
 
-    
+
     return status
-    
+
 }
 
-const removedorProdutos = function(array, nomeProduto){
+const removedorProdutos = function (array, nomeProduto) {
     let nome = nomeProduto
-    let novoArray = array
+    let novoArray = array.slice();
     let indice = novoArray.indexOf(nome);
     let status = false
 
-    if(indice >= 0){
-        const listaProdutosReformulado = novoArray.slice()
-        listaProdutosReformulado.splice(indice,1)
-        console.log(listaProdutosReformulado)
+    if (indice >= 0) {
+        novoArray.splice(indice, 1)
         status = true
-    }else{
+    } else {
         status = false
-        console.log(status)
     }
-    return status
+
+    if (status)
+        return status
+    else
+        return status
 }
 
-removedorProdutos(listaProdutos,'Mousepad')
-console.table(listaProdutos)
+const listagemProdutos = function () {
+
+    // Forma número 1 de como criar um JSON
+    //  let listProdutosJSON = {produtos : listaProdutos, nomes : listaNomes}
+    // let listProdutosJSON = {};
+
+    // Forma número 2 de como criar o json ( adcionando os seus valores no meio do seu codigo)
+
+    // listProdutosJSON.produtos = listaProdutos
+    // listProdutosJSON.nomes = listaNomes
+
+    // console.log(listProdutosJSON)
+    // console.log(listProdutosJSON.produtos[3]);
+    // console.log(listProdutosJSON.nomes[2])
+
+
+    let listProdutosJSON = {}
+    let listProdutosArray = [
+        { nome: 'Monitor', quantidade: 58, marca: 'AlianWare', valor: 2700, codigo: 1 },
+        { nome: 'Monitor', quantidade: 125, marca: 'ASUS', valor: 1970, codigo: 2 },
+        { nome: 'Teclado', quantidade: 238, marca: 'Husky', valor: 350, codigo: 3 },
+        { nome: 'Teclado', quantidade: 92, marca: 'Logitech', valor: 200, codigo: 4 },
+        { nome: 'Teclado', quantidade: 97, marca: 'Razer', valor: 200, codigo: 5 },
+        { nome: 'Teclado', quantidade: 92, marca: 'Redragon', valor: 200, codigo: 6 },
+        { nome: 'Headset', quantidade: 30, marca: 'HyperX', valor: 700, codigo: 7 },
+        { nome: 'Headset', quantidade: 159, marca: 'Havit', valor: 200, codigo: 8 }
+    ]
+
+    listProdutosJSON.produtos = listProdutosArray
+
+    //Arrays para cores 
+    let listCoresAlianwareArray = ['Preto', 'Branco', 'Cinza'];
+    let listCoresAsusArray = ['Preto', 'Cinza'];
+    let listCoresTecladoArray = ['Preto', 'Branco', 'Rosa', 'Azul']
+    let listCoresHeadsetArray = ['Preto', 'Branco', 'Azul', 'Rosa', 'Amarelo', 'Roxo', 'Vermelho']
+
+    //Arrays para modelos 
+    let listModeloMonitorArray = ['LCD', 'LED', 'OLED', '4K', 'IPS']
+    let listModeloTeclado = ['Mecânico', 'Semi=mecânico', 'Membrama', 'Óptico']
+    
+
+
+    listProdutosJSON.produtos[0].cores = listCoresAlianwareArray
+    listProdutosJSON.produtos[1].cores = listCoresAsusArray
+    listProdutosJSON.produtos[2].cores = listCoresTecladoArray
+    listProdutosJSON.produtos[3].cores = listCoresTecladoArray
+    listProdutosJSON.produtos[4].cores = listCoresTecladoArray
+    listProdutosJSON.produtos[5].cores = listCoresTecladoArray
+    listProdutosJSON.produtos[6].cores = listCoresHeadsetArray
+    listProdutosJSON.produtos[7].cores = listCoresHeadsetArray
+
+    //adicionar modeos ao monitores 
+    listProdutosJSON.produtos[0].modelos = listModeloMonitorArray
+    listProdutosJSON.produtos[1].modelos = listModeloMonitorArray
+
+    //Adcionar modelos do teclado
+    listProdutosJSON.produtos[2].modelos = listModeloTeclado
+    listProdutosJSON.produtos[3].modelos = listModeloTeclado
+    listProdutosJSON.produtos[4].modelos = listModeloTeclado
+    listProdutosJSON.produtos[5].modelos = listModeloTeclado
+
+    let listaDeProdutosQnt = listProdutosJSON.produtos.length
+    
+    for(let contador = 0; contador < listaDeProdutosQnt; contador++){
+        console.log('Nome do produto: ' + listProdutosJSON.produtos[contador].nome + '\n' + 'Marca: ' + listProdutosJSON.produtos[contador].marca + )
+        
+    }
+
+
+    
+    
+
+
+
+    // console.log('Nome do produto: ' + listProdutosJSON.produtos[0].nome)
+    // console.log('Marca: ' + listProdutosJSON.produtos[0].marca)
+    // console.log('Valor: ' + listProdutosJSON.produtos[0].valor)
+    // console.log('Cor: ' + listProdutosJSON.produtos[0].cores[2])
+    // console.log('Modelo: ' + listProdutosJSON.produtos[0].modelos[2])
+    // console.log('Quantida: ' + listProdutosJSON.produtos[0].quantidade)
+    // console.log(listProdutosJSON)
+    // console.log(listProdutosJSON.produtos[1].nome)
+    // console.log(listProdutosJSON.produtos[0].marca)
+    // console.log(listProdutosJSON.produtos[0])
+    // console.log(listProdutosJSON.produtos[1])
+    // console.log(listProdutosJSON.produtos[2])
+    // console.log(listProdutosJSON.produtos[3])
+    // console.log(listProdutosJSON.produtos[4])
+    // console.log(listProdutosJSON.produtos[5])
+    // console.log(listProdutosJSON.produtos[6])
+    // console.log(listProdutosJSON.produtos[7])
+    // console.log(listProdutosJSON.produtos[8])
+}
+// console.log(removedorProdutos(listaProdutos,'Mousepad'))
+// console.table(listaProdutos)
+
+
+listagemProdutos();
